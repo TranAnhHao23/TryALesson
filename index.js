ad1634d7dcfbc6d7c3379dcdc4e84ce3dfe9defb
zMaid <zMaiD@arv.co.th>
2023-05-19 01:13:12 +0700

    minor bug

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ function sendEmail() {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://be-neu-elearning.onrender.com/send-email');
     xhr.setRequestHeader('Content-Type', 'application/json')
-
+    alert("Chúng tôi đã tiếp nhận thông tin. Chúng tôi sẽ liên lạc với bạn trong giây lát")
     xhr.onload = () => {
-        if (xhr.status === 200) {
-            alert(xhr.response)
-        } else {
+        if (xhr.status !== 200) {
             alert('Error sending email')
         }
     }
